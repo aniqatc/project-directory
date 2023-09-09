a5b6aceac5386b35f819c4965c6a22bdfd9d0804
aniqatc <aniqatc97@gmail.com>
2023-09-09 11:36:46 -0400

    Move JS into separate file + test demo-link-hover on mobile

---
+++ layout.js
@@ -0,0 +1,21 @@
+/* Use Masonry.js to create dynamic layout containing cards of varying heights */
+document.addEventListener('DOMContentLoaded', function () {
+	const grid = document.querySelector('main');
+	const masonry = new Masonry(grid, {
+		itemSelector: '.card',
+		columnWidth: '.card',
+		gutter: 16,
+		fitWidth: true,
+		stagger: 30,
+		isAnimated: true,
+	});
+
+	masonry.reloadItems();
+
+	imagesLoaded(grid).on('always', function () {
+		masonry.layout();
+	});
+});
+
+/* TEMP: hover interactions on mobile */
+document.addEventListener('touchstart', function () {}, true);
